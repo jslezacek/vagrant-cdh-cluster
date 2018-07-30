a2ced8e118e1e34b18b526b6206d78b9691b1fb2
Jatin Shah <jatin7@gmail.com>
2018-07-30 18:12:01 -0500

    Added common.sh

---
+++ common.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Install prerequisites
+sudo yum -y update
+sudo yum -y install net-tools ntp wget lsof unzip tar iptables-services
+# Enable NTP
+sudo systemctl enable ntpd && sudo systemctl start ntpd
+# Disable Firewall
+sudo systemctl disable firewalld && sudo systemctl stop firewalld
+sudo iptables --flush INPUT && sudo iptables --flush FORWARD && sudo service iptables save
+# Disable SELINUX
+sudo sed -i --follow-symlinks 's/^SELINUX=.*/SELINUX=disabled/g' /etc/sysconfig/selinux
